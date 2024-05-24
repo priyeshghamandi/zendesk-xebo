66830a480e537cc45129da305063f12964851f7f
priyesh <priyesh.ghamandi@questionpro.com>
2024-05-24 14:11:16 +0530

    Changes for heroku

--- app.js
+++ app.js
@@ -11,48 +11,32 @@ const xeboAPI = new Xebo();
 const zendDeskAPI = new Zendesk();
 const sunshineAPI = new Sunshine();
 
-
 app.use(cors());
     app.use(bodyParser.json({limit: '50mb'}));
     app.use(bodyParser.urlencoded({limit: '50mb', extended: false }));
 
-
-
 app.get('/test',function(req,res){
     res.status(200).json({"test": "test"});
 });
-
-
 
 app.post('/zendDesk', async (req,res) => {    
     console.log(req.body)
     const ticketData = req.body;
     const {ticketID, userEmail, metadata} = ticketData;
     const collectorID = "665018e3739e7860660ab6ce";
-    const collectortoken = "P/e0970b57";
-
-    
+    const collectortoken = "P/e0970b57";    
     
     try {
-      const urlResponse = await xeboAPI.getSurveyURL(collectorID,collectortoken, userEmail, metadata);
-      const surveyURLMessage = urlResponse.data[0].message
-      
+      const urlResponse = await xeboAPI.getSurveyURL(collectorID,collectortoken, userEmail, metadata);      
+      const surveyURLMessage = urlResponse.data[0].message      
       const ticketAuditData = await zendDeskAPI.getTicketAudit(ticketID);
-      const conversationID = ticketAuditData.audits[0].events[0].value.conversation_id;
-      
-      const sunshineData = await sunshineAPI.sendMessage(conversationID, surveyURLMessage);
-      
+      const conversationID = ticketAuditData.audits[0].events[0].value.conversation_id;      
+      const sunshineData = await sunshineAPI.sendMessage(conversationID, surveyURLMessage);      
       res.status(200).json(sunshineData);
-
-
-
     }catch (error) {
-      console.log(error);
+      console.error(error);
       res.status(500).json({"error": error});
-
     }
-
-       
 
 });
 
